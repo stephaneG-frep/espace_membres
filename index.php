bf416ecef880b892fd0e3e257130bcdc220e056c
stephaneG-frep <119919153+stephaneG-frep@users.noreply.github.com>
2023-12-06 10:17:17 +0100

    nouvelle pharse

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
 </head><body>
 </div>
 <h1 class="text-center text-white pt-5">Page d'Accueil</h1>
+</br>
 <h3 class="text-center text-dark-50 pt-3">Bienvenue sur l'application Loto++</h3>
-<h5 class="text-center text-warning pt-6">
-    Cette application est faite pour les gens qui ne savent pas quels numéros jouer</h5>
+</br>
+<h5 class="text-center text-warning pt-6">Vous n'avez pas d'idées pour les numéros
+    de votre loto?</h5>
+<p class="text-center text-white pt-6">Laissez-vous guider par les boules de l'appli...
+    et Bonne Chance !
+</p>
 
 
 
